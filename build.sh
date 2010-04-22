cb3f11fe84e64f4b4cff61b6f52484ae73e47834
brad kittenbrink <brad@lindenlab.com>
2010-04-21 17:41:09 -0700

    Updated build script and autobuild.xml so it can be executed by 'autobuild build' command
    --HG--
    branch : autobuild-package-skeleton

--- build.sh
+++ build.sh
@@ -1,15 +1,27 @@
-#!/bin/sh -x
+#!/bin/sh
 
-autobuild="$(which autobuild)"
+if [ -z "$autobuild" ] ; then 
+    autobuild="$(which autobuild)"
+fi
 
+# *NOTE: temporary workaround until autobuild is installed on the build farm
 autobuild_installed ()
 {
+    local hardcoded_rev="parabuild-bootstrap"
+    local bootstrap_url="http://pdp47.lindenlab.com/cgi-bin/hgwebdir.cgi/brad/autobuild/archive/$hardcoded_rev.tar.bz2"
+
+    # hg.lindenlab.com is kind of hosed right now.
+    #local hardcoded_rev="c8062b08a710"
+    #local boostrap_url="http://hg.lindenlab.com/brad/autobuild-trunk/get/$hardcoded_rev.bz2"
+
     if [ -z "$autobuild" ] || [ ! -x "$autobuild" ] ; then
-        echo "looking for autobuild in ../autobuild/bin"
-        autobuild="$(dirname $0)/../autobuild/bin/autobuild"
-        if [ -z "$autobuild" ] || [ ! -x "$autobuild" ] ; then
-            echo "failed to find executable autobuild $autobuild" >&2
-            # *TODO - potentially fetch and use autobuild in sandbox here
+        echo "failed to find executable autobuild $autobuild" >&2
+
+        echo "fetching autobuild rev $hardcoded_rev from $bootstrap_url"
+        curl "$bootstrap_url" | tar -xj
+        autobuild="$(pwd)/autobuild-$hardcoded_rev/bin/autobuild"
+        if [ ! -x "$autobuild" ] ; then
+            echo "failed to bootstrap autobuild!"
             return 1
         fi
     fi
