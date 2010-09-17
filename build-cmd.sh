934c7169883b469e33f2624c8fd1f4898c91028a
brad kittenbrink <brad@lindenlab.com>
2010-09-16 17:03:08 -0700

    Fix for compatibility with latest autobuild.

--- build-cmd.sh
+++ build-cmd.sh
@@ -29,28 +29,33 @@ extract "$ZLIB_ARCHIVE"
 
 top="$(pwd)"
 cd "$ZLIB_SOURCE_DIR"
+	stage="$(pwd)/stage"
     case "$AUTOBUILD_PLATFORM" in
         "windows")
             build_sln "contrib/vstudio/vc8/zlibvc.sln" "Debug|Win32"
             build_sln "contrib/vstudio/vc8/zlibvc.sln" "Release|Win32"
-            mkdir -p stage/lib/debug
-            mkdir -p stage/lib/release
+            mkdir -p "$stage/lib/debug"
+            mkdir -p "$stage/lib/release"
             cp "contrib/vstudio/vc8/x86/ZlibStatDebug/zlibstat.lib" \
-                "stage/lib/debug/zlibd.lib"
+                "$stage/lib/debug/zlibd.lib"
             cp "contrib/vstudio/vc8/x86/ZlibStatRelease/zlibstat.lib" \
-                "stage/lib/release/zlib.lib"
+                "$stage/lib/release/zlib.lib"
             mkdir -p "stage/include/zlib"
-            cp {zlib.h,zconf.h} "stage/include/zlib"
+            cp {zlib.h,zconf.h} "$stage/include/zlib"
         ;;
         "darwin")
-            ./configure --prefix="$(pwd)/stage"
+            ./configure --prefix="$stage"
             make
             make install
+			mkdir -p "$stage/include/zlib"
+			mv "$stage/include/"*.h "$stage/include/zlib/"
         ;;
         "linux")
-            CFLAGS="-m32" CXXFLAGS="-m32" ./configure --prefix="$(pwd)/stage"
+            CFLAGS="-m32" CXXFLAGS="-m32" ./configure --prefix="$stage"
             make
             make install
+			mkdir -p "$stage/include/zlib"
+			mv "$stage/include/"*.h "$stage/include/zlib/"
         ;;
     esac
     mkdir -p stage/LICENSES
