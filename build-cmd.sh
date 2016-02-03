2aad8f66978e22a6d3ad4917909cfa5f6bfd4410
Nat Goodspeed <nat@lindenlab.com>
2016-02-03 17:24:49 -0500

    DRTVWR-418: Adapt zlib build for 64-bit on Mac.
    Add requirement for sibling checkout of build-variables repo. Bring in those
    variables.
    
    Make the platform 'case' statement handle windows64, darwin64, linux64 --
    instead of skipping all build activity for those as it did before!
    
    Adapt Mac configure variables CFLAGS and LDFLAGS to vary -arch depending on
    AUTOBUILD_ADDRSIZE. Use LL_BUILD from build-variables/convenience for the
    rest. Fix script bug in which install_name was set after use, causing the
    linker to stamp the dylib with an empty -install_name.
    For AUTOBUILD_ADDRSIZE=64, add --64 switch to configure.
    
    Comment out all Mac Debug build and test activity. (Copy a couple script
    comments from the Debug block to the Release block.)
    Comment out anything to do with contrib/minizip.
    Remove debug and minizip items from the darwin manifest in autobuild.xml.
    
    Use awk to extract the section of the README containing the copyright info,
    rather than using 'tail -n 31', which would break if the README contents
    change with updated versions of the upstream software.

--- build-cmd.sh
+++ build-cmd.sh
@@ -18,6 +18,13 @@ then AUTOBUILD="$(cygpath -u "$AUTOBUILD")"
 fi
 eval "$("$AUTOBUILD" source_environment)"
 
+# For this library, like most third-party libraries, we only care about
+# Release mode, so source build-variables up front.
+build_variables="../build-variables/convenience"
+[ -r "$build_variables" ] || \
+fail "Please clone https://bitbucket.org/lindenlab/build-variables beside this repo."
+source "$build_variables" Release
+
 VERSION_HEADER_FILE="$ZLIB_SOURCE_DIR/zlib.h"
 version=$(sed -n -E 's/#define ZLIB_VERSION "([0-9.]+)"/\1/p' "${VERSION_HEADER_FILE}")
 build=${AUTOBUILD_BUILD_ID:=0}
@@ -26,7 +33,8 @@ echo "${version}.${build}" > "${stage}/VERSION.txt"
 pushd "$ZLIB_SOURCE_DIR"
     case "$AUTOBUILD_PLATFORM" in
 
-        "windows")
+        # ------------------------ windows, windows64 ------------------------
+        windows*)
             load_vsvars
 
             # This invokes cmake only to convert zconf.h.cmakein to zconf.h.
@@ -70,24 +78,67 @@ pushd "$ZLIB_SOURCE_DIR"
             popd
         ;;
 
-        "darwin")
-            # Select SDK with full path.  This shouldn't have much effect on this
-            # build but adding to establish a consistent pattern.
-            sdk=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk/
-            cc_opts="${TARGET_OPTS:--arch i386 -iwithsysroot $sdk -mmacosx-version-min=10.7} -gdwarf-2 -fPIC -DPIC"
-            ld_opts="-Wl,-install_name,\"${install_name}\" -Wl,-headerpad_max_install_names"
-            export CC=clang
+        # ------------------------- darwin, darwin64 -------------------------
+        darwin*)
+            case "$AUTOBUILD_ADDRSIZE" in
+                32)
+                    cc_arch="i386"
+                    cfg_sw=
+                    ;;
+                64)
+                    cc_arch="x86_64"
+                    cfg_sw="--64"
+                    ;;
+            esac
 
             # Install name for dylibs based on major version number
             install_name="@executable_path/../Resources/libz.1.dylib"
 
-            # Debug first
-            CFLAGS="$cc_opts -O0" \
-                LDFLAGS="$ld_opts" \
-                sh -x ./configure --prefix="$stage" --includedir="$stage/include/zlib" --libdir="$stage/lib/debug"
+            cc_opts="${TARGET_OPTS:--arch $cc_arch $LL_BUILD}"
+            ld_opts="-Wl,-install_name,\"${install_name}\" -Wl,-headerpad_max_install_names"
+            export CC=clang
+
+##          # Debug first
+##          CFLAGS="$cc_opts -O0" \
+##              LDFLAGS="$ld_opts" \
+##              sh -x ./configure $cfg_sw --prefix="$stage" --includedir="$stage/include/zlib" --libdir="$stage/lib/debug"
+##          make
+##          make install
+##          cp -a "$top"/libz_darwin_debug.exp "$stage"/lib/debug/libz_darwin.exp
+##
+##          # conditionally run unit tests
+##          if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
+##              # Build a Resources directory as a peer to the test executable directory
+##              # and fill it with symlinks to the dylibs.  This replicates the target
+##              # environment of the viewer.
+##              mkdir -p ../Resources
+##              ln -sf "${stage}/lib/debug"/*.dylib ../Resources
+##
+##              make test
+##
+##              # And wipe it
+##              rm -rf ../Resources
+##          fi
+##
+##          # minizip
+##          pushd contrib/minizip
+##              CFLAGS="$cc_opts -O0" make -f Makefile.Linden all
+##              cp -a libminizip.a "$stage"/lib/debug/
+##              if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
+##                  make -f Makefile.Linden test
+##              fi
+##              make -f Makefile.Linden clean
+##          popd
+##
+##          make distclean
+
+            # Now release
+            CFLAGS="$cc_opts" \
+            LDFLAGS="$ld_opts" \
+                ./configure $cfg_sw --prefix="$stage" --includedir="$stage/include/zlib" --libdir="$stage/lib/release"
             make
             make install
-            cp -a "$top"/libz_darwin_debug.exp "$stage"/lib/debug/libz_darwin.exp
+            cp -a "$top"/libz_darwin_release.exp "$stage"/lib/release/libz_darwin.exp
 
             # conditionally run unit tests
             if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
@@ -95,7 +146,7 @@ pushd "$ZLIB_SOURCE_DIR"
                 # and fill it with symlinks to the dylibs.  This replicates the target
                 # environment of the viewer.
                 mkdir -p ../Resources
-                ln -sf "${stage}/lib/debug"/*.dylib ../Resources
+                ln -sf "${stage}"/lib/release/*.dylib ../Resources
 
                 make test
 
@@ -103,50 +154,21 @@ pushd "$ZLIB_SOURCE_DIR"
                 rm -rf ../Resources
             fi
 
-            # minizip
-            pushd contrib/minizip
-                CFLAGS="$cc_opts -O0" make -f Makefile.Linden all
-                cp -a libminizip.a "$stage"/lib/debug/
-                if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
-                    make -f Makefile.Linden test
-                fi
-                make -f Makefile.Linden clean
-            popd
-
-            make distclean
-
-            # Now release
-            CFLAGS="$cc_opts -O3" \
-                LDFLAGS="$ld_opts" \
-                ./configure --prefix="$stage" --includedir="$stage/include/zlib" --libdir="$stage/lib/release"
-            make
-            make install
-            cp -a "$top"/libz_darwin_release.exp "$stage"/lib/release/libz_darwin.exp
-
-            # conditionally run unit tests
-            if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
-                mkdir -p ../Resources
-                ln -sf "${stage}"/lib/release/*.dylib ../Resources
-
-                make test
-
-                rm -rf ../Resources
-            fi
-
-            # minizip
-            pushd contrib/minizip
-                CFLAGS="$cc_opts -O3" make -f Makefile.Linden all
-                cp -a libminizip.a "$stage"/lib/release/
-                if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
-                    make -f Makefile.Linden test
-                fi
-                make -f Makefile.Linden clean
-            popd
+##          # minizip
+##          pushd contrib/minizip
+##              CFLAGS="$cc_opts -O3" make -f Makefile.Linden all
+##              cp -a libminizip.a "$stage"/lib/release/
+##              if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
+##                  make -f Makefile.Linden test
+##              fi
+##              make -f Makefile.Linden clean
+##          popd
 
             make distclean
         ;;            
-            
-        "linux")
+
+        # -------------------------- linux, linux64 --------------------------
+        linux*)
             # Linux build environment at Linden comes pre-polluted with stuff that can
             # seriously damage 3rd-party builds.  Environmental garbage you can expect
             # includes:
@@ -232,16 +254,28 @@ pushd "$ZLIB_SOURCE_DIR"
         ;;
     esac
     mkdir -p "$stage/LICENSES"
-    tail -n 31 README > "$stage/LICENSES/zlib.txt"
-    pushd contrib/minizip
-        mkdir -p "$stage"/include/minizip/
-        cp -a ioapi.h zip.h unzip.h "$stage"/include/minizip/
-        tail -n 22 MiniZip64_info.txt > "$stage/LICENSES/minizip.txt"
-    popd
+    # The copyright info for zlib is the tail end of its README file. Tempting
+    # though it is to write something like 'tail -n 31 README', that will
+    # quietly fail if the length of the copyright notice ever changes.
+    # Instead, look for the section header that sets off that passage and copy
+    # from there through EOF. (Given that END is recognized by awk, you might
+    # reasonably expect '/pattern/,END' to work, but no: END can only be used
+    # to fire an action past EOF. Have to simulate by using another regexp we
+    # hope will NOT match.)
+    awk '/^Copyright notice:$/,/@%rest%@/' README > "$stage/LICENSES/zlib.txt"
+    # In case the section header changes, ensure that zlib.txt is non-empty.
+    # (With -e in effect, a raw test command has the force of an assert.)
+    # Exiting here means we failed to match the copyright section header.
+    # Check the README and adjust the awk regexp accordingly.
+    [ -s "$stage/LICENSES/zlib.txt" ]
+##  pushd contrib/minizip
+##      mkdir -p "$stage"/include/minizip/
+##      cp -a ioapi.h zip.h unzip.h "$stage"/include/minizip/
+##      tail -n 22 MiniZip64_info.txt > "$stage/LICENSES/minizip.txt"
+##  popd
 popd
 
 mkdir -p "$stage"/docs/zlib/
 cp -a README.Linden "$stage"/docs/zlib/
 
 pass
-
